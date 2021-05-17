26ed03285daf34da7d72bd0b011215b6a62e3d77
Shlomi <vgtvgy1@gmail.com>
2021-05-18 01:21:03 +0300

    Basic xterm.js works

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Terminal } from 'xterm';
+import '../node_modules/xterm/css/xterm.css'
+
 
 class App extends Component {
   render() {
@@ -11,10 +14,19 @@ class App extends Component {
           <h2>Welcome to React/Electron</h2>
         </div>
         <p className="App-intro">
-          Hello Electron!
+          Hello Electron! Shlomid
         </p>
+		<div id="terminal"></div>
       </div>
     );
+  }
+
+  componentDidMount() {
+	console.log("Test")
+
+	var term = new Terminal();
+	term.open(document.getElementById("terminal"));
+	term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
   }
 }
 
